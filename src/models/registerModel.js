2c23700140a7196bcc60ac6ceb39804ac763558d
VRLucas <lukkita.lukkita@gmail.com>
2023-09-20 13:43:54 -0300

    Criando Usuario 2

--- src/models/registerModel.js
+++ src/models/registerModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validador = require('validator');
 const bcrytjs = require('bcryptjs');
+const { async } = require('regenerator-runtime');
 
 const RegisterSchema = new mongoose.Schema({
     email: { type: String, required: true },
@@ -18,15 +19,26 @@ class Register {
     async registe() {
         this.valida();
         if (this.erros.length > 0) return;
+        await this.userExists();
+        if (this.erros.length > 0) return;
+        const salt = bcrytjs.genSaltSync();
+
+        this.body.senha = bcrytjs.hashSync(this.body.senha, salt);
         try {
-            const salt = bcrytjs.genSaltSync();
-            this.body.senha = bcrytjs.hashSync(this.body.senha, salt);
+
             this.user = await RegisterModel.create(this.body);
         } catch (e) {
             console.log(e);
         }
 
+
     }
+    async userExists() {
+        const user = await RegisterModel.findOne({ email: this.body.email });
+        if (user) this.erros.push('Usuário já existe');
+
+    }
+
     valida() {
         this.cleanUp()
         if (!validador.isEmail(this.body.email)) this.erros.push('Email Inválido');
